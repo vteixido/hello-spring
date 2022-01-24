5785606879583483d411133a0feb5e0b43dae661
veronica T <vteixido@hotmail.com>
2022-01-24 14:09:06 +0100

    Add init method mapped to /

--- src/main/java/com/gft/demo/DemoProjectApplication.java
+++ src/main/java/com/gft/demo/DemoProjectApplication.java
@@ -14,6 +14,11 @@ public class DemoProjectApplication {
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String init() {
+		return "Welcome to Hello-spring";
+	}
+	
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
