dbbbcc7c50b8373924e49b8c87abe10d70c710c5
veronica T <vteixido@hotmail.com>
2022-01-24 13:34:37 +0100

    Added method hello() mapped to /hello

--- src/main/java/com/gft/demo/DemoProjectApplication.java
+++ src/main/java/com/gft/demo/DemoProjectApplication.java
@@ -2,12 +2,20 @@ package com.gft.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
 }
