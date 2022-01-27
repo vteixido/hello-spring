ad39628d9c51fe3ab20cfc81d9a6b338d520b298
veronica T <vteixido@hotmail.com>
2022-01-27 12:47:11 +0100

    Added rootTest innn DemoProjectApplicationTests.java

--- src/test/java/com/gft/demo/DemoProjectApplicationTests.java
+++ src/test/java/com/gft/demo/DemoProjectApplicationTests.java
@@ -1,13 +1,25 @@
 package com.gft.demo;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class DemoProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void rooTest(@Autowired TestRestTemplate restTemplate) {
+		assertThat(restTemplate.getForObject("/", String.class))
+				.isEqualTo("Welcome to Hello-spring");
 	}
 
 }
