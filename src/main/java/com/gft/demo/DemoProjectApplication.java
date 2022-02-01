57f80877eebaba0e91f47b05322a5816d4963dfa
veronica T <vteixido@hotmail.com>
2022-02-01 09:53:31 +0100

    Added divide & subtract methods

--- src/main/java/com/gft/demo/DemoProjectApplication.java
+++ src/main/java/com/gft/demo/DemoProjectApplication.java
@@ -2,17 +2,17 @@ package com.gft.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import javax.annotation.processing.Generated;
 
 @SpringBootApplication
 @RestController
 public class DemoProjectApplication {
 
+	@Generated(value="org.springframework.boot")
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
 	}
@@ -30,26 +30,52 @@ public class DemoProjectApplication {
 	@GetMapping("/add")
 	public Object add(@RequestParam(value="a", defaultValue = "0") Float a,  @RequestParam(value="b", defaultValue = "0") Float b ) {
 
-		Float sum = a+b;
-		Float decimal = sum - sum.intValue();
+		float sum = a+b;
+		float decimal = sum - (int) sum;
 
 		if(decimal  !=  0) {
 			return sum;
 		}else{
-			return Integer.valueOf(sum.intValue());
+			return (int) sum;
 		}
 	}
 
 	@GetMapping("/multiply")
 	public Object multiply(@RequestParam(value="a", defaultValue = "0") Float a,  @RequestParam(value="b", defaultValue = "0") Float b ) {
 
-		Float product = a*b;
-		Float decimal = product - product.intValue();
+		float product = a*b;
+		float decimal = product - (int) product;
 
 		if(decimal  !=  0) {
 			return product;
 		}else{
-			return Integer.valueOf(product.intValue());
+			return (int) product;
+		}
+	}
+
+	@GetMapping("/sub")
+	public Object subtract(@RequestParam(value="a", defaultValue = "0") Float a, @RequestParam(value="b", defaultValue = "0") Float b ) {
+
+		float subtract = a - b;
+		float decimal = subtract - (int) subtract;
+
+		if(decimal  !=  0) {
+			return subtract;
+		}else{
+			return (int) subtract;
+		}
+	}
+
+	@GetMapping("/divide")
+	public Object divide(@RequestParam(value="a", defaultValue = "0") Float a, @RequestParam(value="b", defaultValue = "0") Float b ) {
+
+		float divide = a / b;
+		float decimal = divide - (int) divide;
+
+		if(decimal  !=  0) {
+			return divide;
+		}else{
+			return (int) divide;
 		}
 	}
 }
